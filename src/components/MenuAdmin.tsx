55c15426b24b5933fd146949e5b107bf5a05cfcc
tancd <caotan1008@gmail.com>
2022-03-29 09:17:53 +0700

    Menu Admin

--- src/components/MenuAdmin.tsx
+++ src/components/MenuAdmin.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 type Props = {}
 
 const MenuAdmin = (props: Props) => {
   return (
     <div>
-        <div className='float-left bg-dark'>
-            
+        <div className='float-start bg-dark menu-left  '>
+            <h1 className='text-white'>CAo</h1>
+            <ul className='navbar-nav me-auto mb-2 mb-lg-0 '>
+                <li className='nav-item'>
+                    
+                    <NavLink className="nav-link text-white" aria-current="page" to="/admin/dashboard">Dashboard</NavLink>
+                </li>
+                <li>
+                <NavLink className="nav-link text-white" aria-current="page" to="/admin/dashboard">Dashboard</NavLink>
+                </li>
+                <li>
+                <NavLink className="nav-link text-white" aria-current="page" to="/admin/dashboard">Dashboard</NavLink>
+                </li>
+                <li>
+                <NavLink className="nav-link text-white" aria-current="page" to="/admin/dashboard">Dashboard</NavLink>
+                </li>
+            </ul>
         </div>
-        <div >
-            <h2>Tan</h2>
+        <div className='bg-dark'>
+            <h2 className='text-white'>Tan</h2>
         </div>
     </div>
   )
