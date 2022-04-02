1d46c2cbf2167faf03831af6029fa06345b50b12
tancd <caotan1008@gmail.com>
2022-04-02 17:40:22 +0700

    Admin layout

---
+++ src/components/HeaderAdmin.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const HeaderAdmin = (props: Props) => {
+  return (
+    <div>
+        <div className='bg-dark text-white p-2'>HeaderAdmin</div>
+    </div>
+  )
+}
+
+export default HeaderAdmin
--- src/pages/layout/AdminLayout.tsx
+++ src/pages/layout/AdminLayout.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import HeaderAdmin from '../../components/HeaderAdmin'
+import MenuAdmin from '../../components/MenuAdmin'
 
 type Props = {}
 
 const AdminLayout = (props: Props) => {
   return (
-    <div>AdminLayout</div>
+    <div><div >
+    <header >
+      <HeaderAdmin/>
+    </header>
+    <div className='flex'>
+    <aside >
+      <MenuAdmin/>
+    </aside>
+    <main className='w-100' >
+      <Outlet/>
+    </main>
+    </div> 
+</div></div>
   )
 }
 
