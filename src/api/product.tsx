c95522ee172c7e8508c56ccbeb0a95e04eed0cd5
tancd <caotan1008@gmail.com>
2022-04-02 09:43:32 +0700

    add product

--- src/api/product.tsx
+++ src/api/product.tsx
@@ -14,7 +14,7 @@ export const add = (product:ProductType) => {
     return instance.post(url, product);
 }
 export const update = (product: ProductType) => {
-    const url = `/products/${product.id}`;
+    const url = `/products/${product._id}`;
     return instance.put(url, product);
 }
 export const read = (id: number) => {
--- src/pages/ProductAdd.tsx
+++ src/pages/ProductAdd.tsx
@@ -1,46 +1,81 @@
-import React from 'react'
+import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { ProductType } from '../types/product';
-import { useNavigate } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid';
+import { ProductType } from "../types/product";
+import { useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 type ProductAddProps = {
-    onAdd: (product: ProductType) => void
-}
+    onAdd: (product: ProductType) => void;
+};
 type FormValues = {
-    _id: number,
-    name: string,
-    price: number
-}
-
-
+    // _id: number;
+    name: string;
+    price: number;
+    img:string
+};
 
 const ProductAdd = (props: ProductAddProps) => {
-    const _id = +uuidv4()
+    // const _id = +uuidv4();
     const navigate = useNavigate();
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<FormValues>();
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<FormValues>();
     const onSubmit: SubmitHandler<FormValues> = (data) => {
-        // props.onAdd(data);
+        console.log(data);
+        props.onAdd(data);
         // navigate('/admin/product')
-        console.log(data)
-    console.log(_id)
+        // console.log(data);
+        // console.log(_id);
+    };
+    return (
+        <div className="text-center">
+            <h1>Thêm mới sản phẩm</h1>
+            <form onSubmit={handleSubmit(onSubmit)} className="w-75  mx-auto">
+                <div className="mb-b">
+                    <div className="float-start">
+                        <label htmlFor="">Tên sản phẩm</label>
+                    </div>
+                    <input
+                        type="text"
+                        className="form-control"
+                        {...register("name", { required: true, minLength: 5 })}
+                        placeholder="Nhap ten san pham"
+                    />
+                    {errors.name && errors.name.type === "required" && (
+                        <span className="text-danger">Required</span>
+                    )}
+                    {errors.name && errors.name.type === "minLength" && (
+                        <span className="text-danger">MinLength</span>
+                    )}
+                </div>
+                <div className="mb-3">
+                    <div className="float-start">
+                        <label htmlFor="">Giá</label>
+                    </div>
+                    <input
+                        type="number"
+                        className="form-control"
+                        {...register("price")}
+                        placeholder='Nhap gia'
+                    />
+                </div>
+                <div className="mb-3">
+                    <div className="float-start">
+                        <label htmlFor="">Giá</label>
+                    </div>
+                    <input
+                        type="number"
+                        className="form-control"
+                        {...register("img")}
+                        placeholder='Nhap gia'
+                    />
+                </div>
+                <button className="btn btn-success">Thêm mới sản phẩm</button>
+            </form>
+        </div>
+    );
+};
 
-
-    }
-  return (
-    <div>
-        <form onSubmit={handleSubmit(onSubmit)} className='w-75' >
-            <div className='mb-b'>
-            <input type="text" className='form-control' {...register('name', { required: true, minLength: 5})} />
-            {errors.name && errors.name.type === "required" && <span className='text-danger'>Required</span> }
-            {errors.name && errors.name.type === "minLength" && <span className='text-danger'>MinLength</span> }
-            </div>
-            <div className='mb-3'>
-            <input type="number" className='form-control' {...register('price')}/>
-            </div>
-            <button className='btn btn-success'>Add</button>
-        </form>
-    </div>
-  )
-}
-
-export default ProductAdd
+export default ProductAdd;
