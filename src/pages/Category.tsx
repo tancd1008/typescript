566f94b52f9d57cb8fdc97cd699002b8a2d4722a
tancd <caotan1008@gmail.com>
2022-04-06 14:05:25 +0700

    category and check auth

---
+++ src/pages/Category.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { readCate } from '../api/category';
+import { CateType } from '../types/categories'
+import { ProductType } from '../types/product';
+
+type ProductProps = {
+}
+
+const Category = (props: ProductProps) => {    
+  const [products,setProducts] = useState<ProductType>();
+  const { id } = useParams();
+
+  useEffect(() => {
+      const getCate = async () => {
+          const {data} = await readCate(id)
+          setProducts(data);
+      }
+      getCate()
+
+  },[])
+  return (
+      
+    <div>
+        <div className='row '>
+            {products && products.map((item,index) => {
+              
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default Category
